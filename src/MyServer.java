a24dceeca119acab8ffec329340cdd329dd340b5
ahmadsarvmeily <as20g16@ecs.soton.ac.uk>
2017-11-22 22:26:43 +0000

    Implemented single-client private key exchange

--- src/MyServer.java
+++ src/MyServer.java
@@ -1,6 +1,38 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class MyServer {
 
-    public static void main(String[] args) {
+    //TODO: Support multiple clients concurrently
 
+    public static void main(String[] args) {
+        int port = 5137;
+        int prime = 191;
+        int primitiveRoot = 131;
+
+        ServerSocket serverSocket;
+        Socket clientSocket;
+        KeyGenerator generator = new KeyGenerator(prime, primitiveRoot);
+        try {
+
+            serverSocket = new ServerSocket(port);
+            clientSocket = serverSocket.accept();
+            OutputStream clientOutputStream = clientSocket.getOutputStream();
+            InputStream clientInputStream = clientSocket.getInputStream();
+
+            int x = generator.generateValue();
+            clientOutputStream.write(x);
+            clientOutputStream.write(prime);
+            clientOutputStream.write(primitiveRoot);
+
+            int y = clientInputStream.read();
+            int key = generator.generateKey(y);
+            System.out.println(key);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
