28f903b4884a0eee95e802c10195cacd71ff4a7c
ahmadsarvmeily <as20g16@ecs.soton.ac.uk>
2017-12-09 13:37:07 +0000

    Created CipherTextProxy class to isolate communication with cipher text RMI server

---
+++ src/CipherTextProxy.java
@@ -0,0 +1,27 @@
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+public class CipherTextProxy {
+
+    private CiphertextInterface ctstub;
+
+    CipherTextProxy() {
+        try {
+            System.setProperty( "java.security.policy", "mypolicy" );
+            if (System.getSecurityManager() == null) {
+                System.setSecurityManager(new SecurityManager());
+            }
+            Registry ctreg = LocateRegistry.getRegistry( "svm-tjn1f15-comp2207.ecs.soton.ac.uk", 12345 );
+            ctstub = (CiphertextInterface) ctreg.lookup( "CiphertextProvider" );
+        }
+        catch (NotBoundException | RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String get(String userID, int key) throws RemoteException {
+        return ctstub.get(userID,key);
+    }
+}
