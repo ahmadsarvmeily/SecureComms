2a2522d49e86ee7601ecc06f2330d15d5f81ee92
ahmadsarvmeily <as20g16@ecs.soton.ac.uk>
2017-12-09 13:35:47 +0000

    Renamed primitiveRoot to primRoot

--- src/KeyGenerator.java
+++ src/KeyGenerator.java
@@ -3,18 +3,18 @@ import java.util.Random;
 
 class KeyGenerator {
 
-    private BigInteger prime, primitiveRoot;
+    private BigInteger prime, primRoot;
     private int privateValue;
 
-    KeyGenerator(int prime, int primitiveRoot) {
+    KeyGenerator(int prime, int primRoot) {
         Random random = new Random();
         this.prime = BigInteger.valueOf(prime);
-        this.primitiveRoot = BigInteger.valueOf(primitiveRoot);
+        this.primRoot = BigInteger.valueOf(primRoot);
         privateValue = random.nextInt(1000);
     }
 
     int generateValue() {
-        BigInteger calc = primitiveRoot.pow(privateValue);
+        BigInteger calc = primRoot.pow(privateValue);
         return calc.mod(prime).intValue();
     }
 
